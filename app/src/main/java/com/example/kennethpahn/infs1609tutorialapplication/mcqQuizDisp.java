980f732b9e377f401927ea77ed3101e6bddff215
Kenneth Pahn <z5114063@kennethpahn.info>
2018-10-24 20:18:58 +1100

    readying more api functions
    for mcq and tfreview

--- app/src/main/java/com/example/kennethpahn/infs1609tutorialapplication/mcqQuizDisp.java
+++ app/src/main/java/com/example/kennethpahn/infs1609tutorialapplication/mcqQuizDisp.java
@@ -1,6 +1,7 @@
 package com.example.kennethpahn.infs1609tutorialapplication;
 
 import android.content.Intent;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,15 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 
 public class mcqQuizDisp extends AppCompatActivity {
     private TextView questionTxt;
@@ -26,6 +36,15 @@ public class mcqQuizDisp extends AppCompatActivity {
     // this is to hold the marks
     private int mark;
     private int total;
+    // stolen from https://mobilesiri.com/json-parsing-in-android-using-android-studio/
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
     private mcqQuizContent[] populateMcqQuiz(int moduleNo){
         mcqQuizContent[] mcqQuiz = new mcqQuizContent[10];
         if (moduleNo == 0){
@@ -43,9 +62,30 @@ public class mcqQuizDisp extends AppCompatActivity {
         return mcqQuiz;
     }
     // handle checking answers for us
-    private int checkAnswer(int user, int answer){
+    private int checkAnswer(int user, int answer, int zid) throws IOException {
         if (user == answer) {
             if (add == true) {
+                //http://feewka.kennethpahn.info/recordtf.php?zid=5114063&module_id=0&module_question=0&answer=1
+                // Stolen from https://developer.android.com/reference/android/os/StrictMode
+                // Used to allow http to run on main thread for json.
+                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                        .permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+                // uses php to register users.
+                String url = "http://feewka.kennethpahn.info/recordmcq.php?zid=" + zid + "&module_id=" + moduleNo + "&module_question=" + counter + "&answer=1";
+                URL url2 = new URL(url);
+                InputStream input = (url2).openStream();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(input, Charset.forName("UTF-8")));
+                String status = readAll(rd);
+                // intent to show successful registration.
+                System.out.println("Status: " + status);
+                if (status != ""){
+                    Toast.makeText(getApplicationContext(), "Answer saved.",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Answer save failed.",
+                            Toast.LENGTH_LONG).show();
+                }
                 mark++;
                 total++;
             }
@@ -89,6 +129,7 @@ public class mcqQuizDisp extends AppCompatActivity {
         // get intent details
         Bundle infoPassed = getIntent().getExtras();
         moduleNo = infoPassed.getInt("moduleNo");
+        final int zid = infoPassed.getInt("zid");
         // populate details based on that.
         final mcqQuizContent[] mcqQuiz = populateMcqQuiz(moduleNo);
         questionTxt.setText(mcqQuiz[counter].getQuestion());
@@ -103,7 +144,7 @@ public class mcqQuizDisp extends AppCompatActivity {
             public void onClick(View v){
                 if (aOption.isChecked() == true){
                     try {
-                        if (checkAnswer(0, mcqQuiz[counter].getSolution()) == 1) {
+                        if (checkAnswer(0, mcqQuiz[counter].getSolution(), zid) == 1) {
                             // load next questions and answers
                             questionTxt.setText(mcqQuiz[counter].getQuestion());
                             String[] answers = mcqQuiz[counter].getAnswers();
@@ -120,7 +161,7 @@ public class mcqQuizDisp extends AppCompatActivity {
                 }
                 if (bOption.isChecked() == true){
                     try {
-                        if (checkAnswer(1, mcqQuiz[counter].getSolution()) == 1) {
+                        if (checkAnswer(1, mcqQuiz[counter].getSolution(), zid) == 1) {
                             // load next questions and answers
                             questionTxt.setText(mcqQuiz[counter].getQuestion());
                             String[] answers = mcqQuiz[counter].getAnswers();
@@ -137,7 +178,7 @@ public class mcqQuizDisp extends AppCompatActivity {
                 }
                 if (cOption.isChecked() == true){
                     try {
-                        if (checkAnswer(2, mcqQuiz[counter].getSolution()) == 1) {
+                        if (checkAnswer(2, mcqQuiz[counter].getSolution(), zid) == 1) {
                             // load next questions and answers
                             questionTxt.setText(mcqQuiz[counter].getQuestion());
                             String[] answers = mcqQuiz[counter].getAnswers();
@@ -154,7 +195,7 @@ public class mcqQuizDisp extends AppCompatActivity {
                 }
                 if (dOption.isChecked() == true) {
                     try {
-                        if (checkAnswer(3, mcqQuiz[counter].getSolution()) == 1) {
+                        if (checkAnswer(3, mcqQuiz[counter].getSolution(), zid) == 1) {
                             // load next questions and answers
                             questionTxt.setText(mcqQuiz[counter].getQuestion());
                             String[] answers = mcqQuiz[counter].getAnswers();
