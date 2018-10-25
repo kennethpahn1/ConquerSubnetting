70ec87bf2da4d722cd53a7673cef58b58cb1a3e2
MEECHUM\Kenneth Pahn <z5114063@kennethpahn.info>
2018-10-25 22:27:38 +1100

    remove api

--- api/save.php
+++
@@ -1,8 +0,0 @@
-<?php
-	// saves a user status
-	$con=mysqli_connect("127.0.0.1","feewka","fakepassword","feewka");
-	$sql="INSERT INTO progress (zid, module_id, section, `order`) VALUES ('" . $_GET['zid'] . "', '" . $_GET['module_id'] . "','" . $_GET['section'] . "','" . $_GET['order'] . "') ON DUPLICATE KEY UPDATE `order`='" . $_GET['order'] . "', section='" . $_GET['section'] . "'";
-	if (mysqli_query($con,$sql)) {
-		echo "Success";
-	}
-?>
