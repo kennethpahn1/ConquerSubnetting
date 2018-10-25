201df9fa03635d6a1ece5cadc8bd454fa8fb8128
Jed Ong <jedid75@gmail.com>
2018-10-25 16:11:28 +1100

    UI improvements and some content
    UI improvements and some content

--- app/src/main/java/com/example/kennethpahn/infs1609tutorialapplication/LaunchActivity.java
+++ app/src/main/java/com/example/kennethpahn/infs1609tutorialapplication/LaunchActivity.java
@@ -18,7 +18,7 @@ public class LaunchActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
-                startActivity(new Intent(LaunchActivity.this, IntroActivity.class));
+                startActivity(new Intent(LaunchActivity.this, login.class));
                 finish();
             }
         }, launch_time);
--- app/src/main/java/com/example/kennethpahn/infs1609tutorialapplication/contentDisp.java
+++ app/src/main/java/com/example/kennethpahn/infs1609tutorialapplication/contentDisp.java
@@ -29,7 +29,31 @@ public class contentDisp extends AppCompatActivity {
             moduleContentArray[1] = moduleContent2.getContent();
             moduleContentArray[2] = moduleContent3.getContent();
             moduleContentArray[3] = moduleContent4.getContent();
+        } else if(moduleNo == 1){
+            moduleContent moduleContent1 = new moduleContent(1, 1, 1, "It is not immediately obvious which portion of an IP address belongs to the network, and which portion belongs to the host. \n" +
+                    "\n" +
+                    "In networking, we use subnet masks to differentiate networks and hosts. \n" +
+                    "A subnet mask is essentially a string of 1s and 0s which helps us do this. 1s represent the network portion of the address, while 0s represent hosts. \n");
+            moduleContent moduleContent2 = new moduleContent(2, 1, 2, "To illustrate an example of this, let’s take a look at the following IP address and subnet mask.\n\n" +
+                    "Address: 192.168.15.1\t (in bits; 11000000.10101000.00001111.00000001)\n" +
+                    "Subnet mask: 255.255.255.0  (in bits; 11111111.11111111.11111111.00000000)\n\n" +
+                    "The above IP address shows the address of a host in the network 192.168.15.0. The subnet mask essentially tells us the first 3 numbers, 192.168.15, is the host portion of the address, as the subnet mask occupies the first 24 bits of the address. \n");
+            moduleContent moduleContent3 = new moduleContent(3, 1, 3, "IPv4 addresses are 32-bit addresses that allows information flow to be controlled and redirected/routed. Some of the features of IP addressing are DHCP, NAT and ARP - all of which play critical functions in networking (which we won’t be covering in this subnetting course).\n");
+            moduleContent moduleContent4 = new moduleContent(4, 1, 4, "IP addresses have a network portion and a host portion, which simply distinguishes between whether the portion belongs to a network or host. All devices/hosts in the same broadcast domain must have the same network portion.");
+            // index them
+            moduleContentArray[0] = moduleContent1.getContent();
+            moduleContentArray[1] = moduleContent2.getContent();
+            moduleContentArray[2] = moduleContent3.getContent();
+            moduleContentArray[3] = moduleContent4.getContent();
+        } else if(moduleNo == 2){
+
+
+
         }
+
+
+
+
         return moduleContentArray;
     }
     @Override
